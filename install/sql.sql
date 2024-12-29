dbc06f98b3f73b2f9cc7ea519c82bd418c905421
illama <10000maxence@gmail.com>
2024-12-29 13:55:25 -0500

    Update sql.sql

--- install/sql.sql
+++ install/sql.sql
@@ -9,34 +9,15 @@ CREATE TABLE `illama_bills` (
   `type` enum('society','personal') DEFAULT NULL,
   `society` varchar(60) DEFAULT NULL,
   `status` enum('pending','paid','deleted') DEFAULT 'pending',
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `tags` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT '[]' CHECK (json_valid(`tags`))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 ALTER TABLE `illama_bills`
   ADD PRIMARY KEY (`id`);
 
-
 ALTER TABLE `illama_bills`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-COMMIT;
-
-CREATE TABLE `illama_recurring_payments` (
-  `id` int(11) NOT NULL,
-  `recurring_bill_id` int(11) DEFAULT NULL,
-  `amount` int(11) DEFAULT NULL,
-  `payments_count` int(11) DEFAULT NULL,
-  `payment_date` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-ALTER TABLE `illama_recurring_payments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `recurring_bill_id` (`recurring_bill_id`);
-
-ALTER TABLE `illama_recurring_payments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
-ALTER TABLE `illama_recurring_payments`
-  ADD CONSTRAINT `illama_recurring_payments_ibfk_1` FOREIGN KEY (`recurring_bill_id`) REFERENCES `illama_recurring_bills` (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 CREATE TABLE `illama_recurring_bills` (
@@ -56,5 +37,24 @@ ALTER TABLE `illama_recurring_bills`
   ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `illama_recurring_bills`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+CREATE TABLE `illama_recurring_payments` (
+  `id` int(11) NOT NULL,
+  `recurring_bill_id` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `payments_count` int(11) DEFAULT NULL,
+  `payment_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `illama_recurring_payments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `recurring_bill_id` (`recurring_bill_id`);
+
+ALTER TABLE `illama_recurring_payments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `illama_recurring_payments`
+  ADD CONSTRAINT `illama_recurring_payments_ibfk_1` FOREIGN KEY (`recurring_bill_id`) REFERENCES `illama_recurring_bills` (`id`);
 COMMIT;
